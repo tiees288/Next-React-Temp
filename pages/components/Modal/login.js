13ebbd26f57835eb72774253d786cf8e49d163b9
Krittawat Boonchaiwattana <tiees288@gmail.com>
2020-12-29 13:37:28 +0700

    Login Page

--- pages/components/Modal/login.js
+++ pages/components/Modal/login.js
@@ -4,28 +4,62 @@ import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
 
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import { colors } from '@material-ui/core';
+
 const useStyles = makeStyles((theme) => ({
     modal: {
         display: 'flex',
         // padding: theme.spacing(1),
         alignItems: 'center',
         justifyContent: 'center',
-      },
+    },
     paper: {
-        width: '55%',
+        width: '50%',
         backgroundColor: theme.palette.background.paper,
-        border:0,
+        border: 0,
         boxShadow: theme.shadows[5],
         outline: 'none',
         padding: theme.spacing(2, 4, 3),
-      },
+    },
+    closeBtn: {
+
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        margin: 'auto',
+        width: '90%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        background: 'black',
+        color: 'white',
+        margin: theme.spacing(3, 0, 2),
+        '&:hover': {
+            background: "#545454",
+        },
+    },
 }));
 
 const LoginModal = (props) => {
     const classes = useStyles();
     const { openLogin, setopenLogin } = props
 
-    console.log('Open : '+openLogin)
+    console.log('Open : ' + openLogin)
 
     // const handleOpen = () => {
     //     setopenLogin(true);
@@ -38,7 +72,7 @@ const LoginModal = (props) => {
     return (
         <div>
             <Modal style={{
-                border:0
+                border: 0
             }}
                 aria-labelledby="transition-modal-title"
                 aria-describedby="transition-modal-description"
@@ -54,10 +88,68 @@ const LoginModal = (props) => {
                 <Fade in={openLogin} style={{
                     border: 0
                 }}>
+
+                    {/* <Container component="main" maxWidth="xs"> */}
+                    {/* <CssBaseline /> */}
                     <div className={classes.paper}>
-                     <h2>Login</h2>
-                        <p id="transition-modal-description">react-transition-group animates me.</p>
+
+                        <form className={classes.form} noValidate>
+                            <Typography component="h2" variant="h5">
+                                Login
+                            </Typography>
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Username"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                            {/* <FormControlLabel
+                                    control={<Checkbox value="remember" color="primary" />}
+                                    label="Remember me"
+                                /> */}
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                            >Sign In
+                                </Button>
+                            <Grid container>
+                                <Grid item xs>
+                                    <Link href="#" variant="body2">
+                                        Forgot password?
+                                        </Link>
+                                </Grid>
+                                <Grid item>
+                                    Don't have an account?
+                                        <Link href="#" variant="body2">
+                                        {" Sign Up"}
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </form>
                     </div>
+                    {/* <Box mt={8}>
+                            <Copyright />
+                        </Box> */}
+                    {/* </Container> */}
                 </Fade>
             </Modal>
         </div>
